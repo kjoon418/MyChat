4521cabf7afc5c1c5e94a99b739590c9293cc32b
Jun <kjoon418@naver.com>
2024-11-20 14:42:41 +0900

    feat: Set the project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # MyChat
+
 JWT, OAuth, Spring을 통한 채팅 서버 구현 연습 목적의 토이 프로젝트
---
+++ src/test/java/junwatson/mychat/MyChatApplicationTests.java
@@ -0,0 +1,13 @@
+package junwatson.mychat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
