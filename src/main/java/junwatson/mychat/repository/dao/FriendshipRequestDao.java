3071748628c47598d551521e99645cfe2104609f
Joon Kim <kjoon418@naver.com>
2024-11-22 12:09:01 +0900

    chore: Add log in FriendshipRequestDao

--- src/main/java/junwatson/mychat/repository/dao/FriendshipRequestDao.java
+++ src/main/java/junwatson/mychat/repository/dao/FriendshipRequestDao.java
@@ -2,14 +2,18 @@ package junwatson.mychat.repository.dao;
 
 import junwatson.mychat.domain.FriendshipRequest;
 import junwatson.mychat.domain.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
+@Slf4j
 public class FriendshipRequestDao {
 
     public boolean isRequestExists(Member member, Member friend) {
+        log.info("FriendshipRequestDao.isRequestExists() called");
+
         Optional<FriendshipRequest> findRequest = member.getReceivedFriendshipRequests()
                 .stream()
                 .filter((request) -> request.getRequestMember().equals(friend))
@@ -19,6 +23,8 @@ public class FriendshipRequestDao {
     }
 
     public void createFriendshipRequest(Member member, Member friend) {
+        log.info("FriendshipRequestDao.createFriendshipRequest() called");
+
         FriendshipRequest friendshipRequest = FriendshipRequest.builder()
                 .requestMember(member)
                 .responseMember(friend)
@@ -31,6 +37,8 @@ public class FriendshipRequestDao {
      * 친구 추가 요청을 삭제하는 메서드
      */
     public void removeRequest(Member member, Member friend) {
+        log.info("FriendshipRequestDao.removeRequest() called");
+
         Optional<FriendshipRequest> findRequest = member.getSentFriendshipRequests().stream()
                 .filter((request) -> request.getResponseMember().equals(friend))
                 .findAny();
