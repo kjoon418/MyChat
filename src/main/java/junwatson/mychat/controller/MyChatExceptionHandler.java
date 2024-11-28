153cb1112e8dd555b39ae1a111e6f512f6b855aa
Joon Kim <kjoon418@naver.com>
2024-11-28 09:13:07 +0900

    chore: Update MyChatExceptionHandler

--- src/main/java/junwatson/mychat/controller/MyChatExceptionHandler.java
+++ src/main/java/junwatson/mychat/controller/MyChatExceptionHandler.java
@@ -6,8 +6,7 @@ import org.springframework.http.ResponseEntity;
 
 import java.sql.SQLIntegrityConstraintViolationException;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.*;
 
 @Slf4j
 public class MyChatExceptionHandler {
@@ -32,6 +31,7 @@ public class MyChatExceptionHandler {
             return ResponseEntity.status(BAD_REQUEST).body("다른 사용자가 이미 사용하고 있는 값이거나, 이미 처리된 요청입니다.");
         }
 
-        throw new RuntimeException(e);
+        log.error("Unhandled error occurred", e);
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body("서버 측에 문제가 발생했습니다.");
     }
 }
