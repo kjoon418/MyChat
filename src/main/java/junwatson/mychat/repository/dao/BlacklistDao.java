2363449fc8b1aae8d91f3dc768b0f8cddc377809
Joon Kim <kjoon418@naver.com>
2024-12-01 13:44:00 +0900

    chore: Add log in BlacklistDao

--- src/main/java/junwatson/mychat/repository/dao/BlacklistDao.java
+++ src/main/java/junwatson/mychat/repository/dao/BlacklistDao.java
@@ -3,22 +3,30 @@ package junwatson.mychat.repository.dao;
 import junwatson.mychat.domain.Blacklist;
 import junwatson.mychat.domain.Member;
 import junwatson.mychat.exception.MemberNotExistsException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Slf4j
 public class BlacklistDao {
 
     public boolean isBlocked(Member member, Member target) {
+        log.info("BlacklistDao.isBlocked() called");
+
         return target.getBlacklists().stream()
                 .anyMatch(blacklist -> blacklist.getTargetMember().equals(member));
     }
 
     public boolean isBlacklistExists(Member member, Member target) {
+        log.info("BlacklistDao.isBlacklistExists() called");
+
         return member.getBlacklists().stream()
                 .anyMatch(blacklist -> blacklist.getTargetMember().equals(target));
     }
 
     public Blacklist createBlacklist(Member member, Member target) {
+        log.info("BlacklistDao.createBlacklist() called");
+
         Blacklist blacklist = Blacklist.builder()
                 .member(member)
                 .targetMember(target)
@@ -30,6 +38,8 @@ public class BlacklistDao {
     }
 
     public Blacklist removeBlacklist(Member member, Member target) {
+        log.info("BlacklistDao.removeBlacklist() called");
+
         Blacklist findBlacklist = member.getBlacklists().stream()
                 .filter(blacklist -> blacklist.getTargetMember().equals(target))
                 .findAny()
