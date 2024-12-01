c8393c13bcd9549d9537268fff3d87a0b23fe722
Joon Kim <kjoon418@naver.com>
2024-12-01 11:04:10 +0900

    build: Remove duplicated dependency

--- src/main/java/junwatson/mychat/dto/request/MemberSignUpRequestDto.java
+++ src/main/java/junwatson/mychat/dto/request/MemberSignUpRequestDto.java
@@ -3,16 +3,19 @@ package junwatson.mychat.dto.request;
 import junwatson.mychat.domain.Member;
 import junwatson.mychat.domain.type.MemberAuthorizationType;
 import junwatson.mychat.domain.type.MemberRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import static lombok.AccessLevel.PROTECTED;
+import static lombok.AccessLevel.PUBLIC;
 
 /**
  * 다른 OAuth 를 이용하지 않고, 직접 MyChat 서비스를 통해 회원 가입하는 리퀘스트를 처리하는 DTO
  */
 @Getter
-@NoArgsConstructor(access = PROTECTED)
+@Builder
+@AllArgsConstructor(access = PUBLIC)
 public class MemberSignUpRequestDto {
 
     private String email;
